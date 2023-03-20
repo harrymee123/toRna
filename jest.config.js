427eb8f85e5d21b7155dd5e7fba7c98991d980d0
Harry Mee <harry.mee@giffgaff.co.uk>
2023-03-20 17:33:21 +0000

    inital commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  verbose: true,
+  testURL: "http://localhost/",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
